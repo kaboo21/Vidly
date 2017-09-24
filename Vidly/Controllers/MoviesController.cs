9f299611f25651ec2c59ed175ca62d2505937dea
kabool <kabool21@gmail.com>
2017-09-24 21:46:56 +0300

    Chapter 2 ex. complited

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,25 +10,27 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Random()
+        public ViewResult Index()
         {
-            var movie = new Movie() {Name = "Shrek!"};
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer { Name = "Customer  2"}
-                
-            };
+            var movies = GetMovies();
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers= customers
-            };
-            return View(viewModel);
+            return View(movies);
         }
 
-        
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(c => c.Id == id);
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
     }
 } 
